28e2fb1621b774d777714487c72eca676f78a6d8
NeoUzumaki <87541998+NeoUzumaki@users.noreply.github.com>
2025-01-29 20:10:48 -0300

    Update app.js para uma única função comprar

--- js/app.js
+++ js/app.js
@@ -1,54 +1,29 @@
+document.getElementById('tipo-ingresso').addEventListener('change', updateMaxQty);
 
-function comprar (){
-    let tipo = document.getElementById('tipo-ingresso');
-    let qtd = parseInt(document.getElementById('qtd').value);
-
-    if (qtd < 0){
-        alert('Informe uma quantidade maior que zero');
-        
-    }else{
-        if(tipo.value == 'pista'){
-            comprarPista(qtd);
-        }
-        if(tipo.value == 'superior'){
-            comprarSuperior(qtd);
-        }
-        if(tipo.value == 'inferior'){
-            comprarInferior(qtd);
-        }
-    }
+function comprar() {
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = parseInt(document.getElementById('qtd').value);
     
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert('Por favor, insira uma quantidade válida.');
+        return; 
+    }
+
+    let quantidadeDisponivel = document.getElementById(`qtd-${tipoIngresso}`).innerText;
+    let novaQuantidade = quantidadeDisponivel - quantidade;
+
+    if (novaQuantidade < 0) {
+        alert('Quantidade de ingressos insuficiente.');
+        return;
+    }
+
+    document.getElementById(`qtd-${tipoIngresso}`).innerText = novaQuantidade;
+    alert('Compra realizada com sucesso!');
 }
 
-function comprarPista(qtd){
-    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
-    if (qtd > qtdPista){
-        alert(`Quantidade indisponível para tipo pista`);
-    } else {
-        qtdPista = qtdPista - qtd;
-        document.getElementById('qtd-pista').textContent = qtdPista;
-        alert(`Compra realizada com sucesso!`)
-    }
-}
+function updateMaxQty() {
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let maxQty = parseInt(document.getElementById(`qtd-${tipoIngresso}`).innerText);
 
-function comprarSuperior(qtd){
-    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
-    if (qtd > qtdSuperior){
-        alert(`Quantidade indisponível para tipo superior`);
-    } else {
-        qtdSuperior = qtdSuperior - qtd;
-        document.getElementById('qtd-superior').textContent = qtdSuperior;
-        alert(`Compra realizada com sucesso!`)
-    }
-}
-
-function comprarInferior(qtd){
-    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
-    if (qtd > qtdInferior){
-        alert(`Quantidade indisponível para tipo inferior`);
-    } else {
-        qtdInferior = qtdInferior - qtd;
-        document.getElementById('qtd-inferior').textContent = qtdInferior;
-        alert(`Compra realizada com sucesso!`)
-    }
+    document.getElementById('qtd').max = maxQty;
 }
